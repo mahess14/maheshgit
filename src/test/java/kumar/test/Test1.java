5ca1d5b7c1ed5b90510e06ff2bf8c8ba47388ea1
mahess14 <128316289+mahess14@users.noreply.github.com>
2023-04-04 18:57:21 +0530

    Update Test1.java
    ok commiting

--- src/test/java/kumar/test/Test1.java
+++ src/test/java/kumar/test/Test1.java
@@ -3,7 +3,10 @@ package kumar.test;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("hi test github  mahesh... ok testing" );
+		System.out.println("hi test github  mahesh... ok testing 1" );
+		System.out.println("hi test github  mahesh 2);
+		System.out.println("ok testing 3" );
+		
 
 	}
 
