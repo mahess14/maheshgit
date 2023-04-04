7050e15472bd46a9466c7c8f350b75d24ca5c343
mahess14 <gmksvec14@gmail.com>
2023-04-04 19:25:21 +0530

    committing3

--- src/test/java/kumar/test/Test1.java
+++ src/test/java/kumar/test/Test1.java
@@ -4,11 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("hi test github  mahesh... ok testing 1" );
-		System.out.println("hi test github  mahesh 2);
-		System.out.println("ok testing 3" );
-				   System.out.println("hi test github  mahesh... ok testing 21" );
-		System.out.println("hi test github  mahesh 22);
-		System.out.println("ok testing 23" );
+		
 		
 
 		
