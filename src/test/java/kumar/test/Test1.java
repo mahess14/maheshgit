2fdbf39dd652eb47429775337317bb2cfe2c9b90
sukanyaudumula <129834363+sukanyaudumula@users.noreply.github.com>
2023-04-04 18:59:21 +0530

    Update Test1.java
    test modification

--- src/test/java/kumar/test/Test1.java
+++ src/test/java/kumar/test/Test1.java
@@ -6,6 +6,11 @@ public class Test1 {
 		System.out.println("hi test github  mahesh... ok testing 1" );
 		System.out.println("hi test github  mahesh 2);
 		System.out.println("ok testing 3" );
+				   System.out.println("hi test github  mahesh... ok testing 21" );
+		System.out.println("hi test github  mahesh 22);
+		System.out.println("ok testing 23" );
+		
+
 		
 
 	}
