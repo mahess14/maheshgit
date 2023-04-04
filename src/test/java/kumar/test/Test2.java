189cf4885e3861edd6375429b311b159f2c07bca
mahess14 <gmksvec14@gmail.com>
2023-04-04 19:08:53 +0530

    test2 committing

---
+++ src/test/java/kumar/test/Test2.java
@@ -0,0 +1,16 @@
+package kumar.test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("hi ...");
+		System.out.println("testing ...");
+
+		System.out.println(" github...");
+
+		System.out.println("mahesh...");
+
+
+	}
+
+}
